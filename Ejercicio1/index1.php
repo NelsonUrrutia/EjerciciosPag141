658242aea60ba3f01c803ae33992a5139d4de39d
Nelson E. Urrutia <Nelson E. Urrutia@LAPTOP-PRKLJ65C>
2019-04-27 21:11:21 -0600

    Terminando ejercicio 2

--- Ejercicio1/index1.php
+++ Ejercicio1/index1.php
@@ -9,7 +9,31 @@
 	<header></header>
 	<nav></nav>
 	<aside></aside>
-	<section></section>
+        <section>
+            <form action="#" method="post">
+                Ingrese el número de filas que quiere en su piramide <input type="text" name="n">
+                <br> <input type="submit" name="b1">
+            </form>
+            <br>
+            <?php
+                if (isset($_POST["b1"])) {
+                    $nu=$_POST["n"];                    
+                        for ($i = 1; $i <=$nu; $i++) {
+                            for ($j = 1; $j <=2*$nu-1; $j++) {
+                                 if ($j>=$nu-($i-1) && $j<=$nu+($i-1)){
+                                     echo '*';
+                                 } else {
+                                     print_r("  ");
+                                 }
+                            }
+                            echo '<br>';
+                        }
+                    
+                }
+            
+            ?>
+            
+        </section>
 	<footer></footer>
 	
 </body>
