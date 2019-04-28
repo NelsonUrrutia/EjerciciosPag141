409bda31a9a7eb638907377e50b2d603081857a7
Nelson E. Urrutia <Nelson E. Urrutia@LAPTOP-PRKLJ65C>
2019-04-27 20:49:03 -0600

    Ejercicio 2

---
+++ Ejercicio2/index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewpot" content="width=device-width,Initial-escale=1,Maximun-scale=1">
+</head>
+<body>
+	<header></header>
+	<nav></nav>
+	<aside></aside>
+	<section></section>
+	<footer></footer>
+	
+</body>
+</html>
