56cfbc59481b0ee037dd3f426b377114f9c57ac3
Nelson E. Urrutia <Nelson E. Urrutia@LAPTOP-PRKLJ65C>
2019-05-11 10:44:27 -0600

    Ejercicio pendiente

---
+++ Ejercicio4/index4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ejercicio 2</title>
+	<meta name="viewpot" content="width=device-width,Initial-escale=1,Maximun-scale=1">
+</head>
+<body>
+	<header></header>
+	<nav></nav>
+	<aside></aside>
+        <section>
+ s
+           
+        </section>
+	<footer></footer>
+	
+</body>
+</html>
+
